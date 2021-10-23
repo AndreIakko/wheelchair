407a3c63ab2a43b898959fdd73d8bf476ab9d354
AndreIakko <63969450+AndreIakko@users.noreply.github.com>
2021-10-23 20:26:02 +0200

    Add files via upload

---
+++ iak_wheelchair/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+
+games { 'rdr3', 'gta5' }
+
+author 'Iakkoise'
+
+version '1.0'
+
+files {
+        'data/vehicles.meta',
+        'data/carvariations.meta',
+        'data/handling.meta'
+}
+
+
+
+        data_file 'HANDLING_FILE'               'data/handling.meta'
+        data_file 'VEHICLE_METADATA_FILE'       'data/vehicles.meta'
+        data_file 'VEHICLE_VARIATION_FILE'      'data/carvariations.meta'
