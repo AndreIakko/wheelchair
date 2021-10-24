04600392f258bef2a24f0ae0f65510135d46ea16
Michael S <43636601+MichaelCoding25@users.noreply.github.com>
2021-10-24 08:11:25 +0300

    Update fxmanifest to be more readable
    - Updated the fx_version to latest cerulean
    - Fixed and made it so this script can only run in gta5 as the model would not work in rdr2
    - Added description of resource and URL or github repo
    - Updated minor version
    - Refactored fxmanifest file

--- iak_wheelchair/fxmanifest.lua
+++ iak_wheelchair/fxmanifest.lua
@@ -1,19 +1,19 @@
-fx_version 'bodacious'
-
-games { 'rdr3', 'gta5' }
+fx_version 'cerulean'
+game 'gta5'
 
 author 'Iakkoise'
+description 'A drivable wheelchair'
+url 'https://github.com/AndreIakko/wheelchair'
 
-version '1.0'
+version '1.0.1'
+
 
 files {
-        'data/vehicles.meta',
-        'data/carvariations.meta',
-        'data/handling.meta'
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+	'data/handling.meta'
 }
 
-
-
-        data_file 'HANDLING_FILE'               'data/handling.meta'
-        data_file 'VEHICLE_METADATA_FILE'       'data/vehicles.meta'
-        data_file 'VEHICLE_VARIATION_FILE'      'data/carvariations.meta'
+data_file 'HANDLING_FILE'			'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/carvariations.meta'
